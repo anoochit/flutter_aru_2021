05de7e69c16d745a292643294a1191c40562919f
Anuchit Chalothorn <anoochit@gmail.com>
2021-01-17 19:30:49 +0700

    update trip data

--- 03_travel_guide_ui/lib/model/trip.dart
+++ 03_travel_guide_ui/lib/model/trip.dart
@@ -10,9 +10,31 @@ class Trip {
 
 List<Trip> listTrip = [
   Trip(
-      '1st Time at opera house',
-      'Sydney',
-      'Australia',
-      'Duis mollit eu veniam est dolore anim exercitation ex consectetur. Aute incididunt amet ut tempor. Quis laboris sit id in reprehenderit eiusmod. Consectetur commodo dolore sint fugiat. Consectetur aute anim incididunt nostrud pariatur. Sint ea veniam et sit reprehenderit sunt nulla nisi fugiat quis eiusmod non do. {{1}} Cupidatat aliqua aute mollit velit deserunt in laborum cillum. Incididunt labore minim reprehenderit tempor Lorem. Dolor tempor qui minim et aute veniam laboris aute nulla. Deserunt ea commodo incididunt voluptate nulla. Culpa commodo est enim commodo. Consequat ullamco mollit adipisicing reprehenderit pariatur.',
-      ['assets/images/place06.png', 'assets/images/place05.png']),
+    '1st Time at opera house',
+    'Sydney',
+    'Australia',
+    'Duis mollit eu veniam est dolore anim exercitation ex consectetur. Aute incididunt amet ut tempor. Quis laboris sit id in reprehenderit eiusmod. Consectetur commodo dolore sint fugiat. Consectetur aute anim incididunt nostrud pariatur. Sint ea veniam et sit reprehenderit sunt nulla nisi fugiat quis eiusmod non do. <<1>> Cupidatat aliqua aute mollit velit deserunt in laborum cillum. Incididunt labore minim reprehenderit tempor Lorem. Dolor tempor qui minim et aute veniam laboris aute nulla. <<2>> Deserunt ea commodo incididunt voluptate nulla. Culpa commodo est enim commodo. Consequat ullamco mollit adipisicing reprehenderit pariatur.',
+    ['assets/images/place06.png', 'assets/images/place05.png'],
+  ),
+  Trip(
+    'Firework at the harbor',
+    'Sydney',
+    'Australia',
+    'Duis mollit eu veniam est dolore anim exercitation ex consectetur. Aute incididunt amet ut tempor. Quis laboris sit id in reprehenderit eiusmod. Consectetur commodo dolore sint fugiat. Consectetur aute anim incididunt nostrud pariatur. Sint ea veniam et sit reprehenderit sunt nulla nisi fugiat quis eiusmod non do. <<1>> Cupidatat aliqua aute mollit velit deserunt in laborum cillum. Incididunt labore minim reprehenderit tempor Lorem. Dolor tempor qui minim et aute veniam laboris aute nulla. Deserunt ea commodo incididunt voluptate nulla. Culpa commodo est enim commodo. Consequat ullamco mollit adipisicing reprehenderit pariatur. <<2>>',
+    ['assets/images/place04.png', 'assets/images/place05.png'],
+  ),
+  Trip(
+    'Race into the night',
+    'NewYork',
+    'United State',
+    'Duis mollit eu veniam est dolore anim exercitation ex consectetur. Aute incididunt amet ut tempor. Quis laboris sit id in reprehenderit eiusmod. Consectetur commodo dolore sint fugiat. Consectetur aute anim incididunt nostrud pariatur. Sint ea veniam et sit reprehenderit sunt nulla nisi fugiat quis eiusmod non do. <<1>> Cupidatat aliqua aute mollit velit deserunt in laborum cillum. Incididunt labore minim reprehenderit tempor Lorem. Dolor tempor qui minim et aute veniam laboris aute nulla. Deserunt ea commodo incididunt voluptate nulla. Culpa commodo est enim commodo. Consequat ullamco mollit adipisicing reprehenderit pariatur.',
+    ['assets/images/place01.png', 'assets/images/place05.png'],
+  ),
+  Trip(
+    'The Harbor',
+    'Sydney',
+    'Australia',
+    'Duis mollit eu veniam est dolore anim exercitation ex consectetur. Aute incididunt amet ut tempor. Quis laboris sit id in reprehenderit eiusmod. Consectetur commodo dolore sint fugiat. Consectetur aute anim incididunt nostrud pariatur. Sint ea veniam et sit reprehenderit sunt nulla nisi fugiat quis eiusmod non do. <<1>> Cupidatat aliqua aute mollit velit deserunt in laborum cillum. Incididunt labore minim reprehenderit tempor Lorem. Dolor tempor qui minim et aute veniam laboris aute nulla. Deserunt ea commodo incididunt voluptate nulla. Culpa commodo est enim commodo. Consequat ullamco mollit adipisicing reprehenderit pariatur.',
+    ['assets/images/place06.png', 'assets/images/place05.png'],
+  ),
 ];
